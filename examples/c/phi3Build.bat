969b6a1d0d6edb435a9257a80a2facaf174a5afe
Kshitij Khode <kshitij.khode@intel.com>
2025-07-06 16:48:13 -0700

    Add build script and hacked header contents

---
+++ examples/c/phi3Build.bat
@@ -0,0 +1,20 @@
+set "BUILD_FOLDER=C:\onnxruntime-genai-dev-tools\build_tools\BuildArtifacts-20250624_145252"
+set OVEP_INSTALL=%BUILD_FOLDER%\onnxruntime-install
+
+echo Building onnxruntime-genai ...
+python build.py --config RelWithDebInfo --parallel --skip_tests --ort_home %OVEP_INSTALL% || exit /b 1
+
+echo Building onnxruntime-genai Phi3 C samples...
+cd %BUILD_FOLDER%\onnxruntime-genai
+copy /y %OVEP_INSTALL%\include\* examples\c\include
+copy /y %OVEP_INSTALL%\lib\* examples\c\lib
+copy /y src\ort_genai.h examples\c\include\.
+copy /y src\ort_genai_c.h examples\c\include\.
+copy /y  build\Windows\RelWithDebInfo\RelWithDebInfo\onnxruntime-genai.lib examples\c\lib\.
+copy /y  build\Windows\RelWithDebInfo\RelWithDebInfo\onnxruntime-genai.dll examples\c\lib\.
+
+cd examples/c
+mkdir build
+cd build
+cmake -DPHI3=ON -DPHI3_QA=ON ..
+cmake --build . --config RelWithDebInfo
